b6b80699d9baf91db09505df0026bec26795b985
RPandian <rpandian@ict.gov.sc>
2026-03-08 07:02:31 +0400

    dir change

--- school-frontend/src/App.js
+++ school-frontend/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-// import SchoolSystem from './SchoolSystem';
-// export default function App() { return <SchoolSystem />; }
+import SchoolSystem from './SchoolSystem';
+export default function App() { return <SchoolSystem />; }
 
 function App() {
   return (
